e5d56fb99c1d550abab36c1b0d1f2b804b641973
Jugya K. Gogoi <crjugya123@gmail.com>
2023-07-04 19:17:11 +0530

    Remove: Comments

--- game.js
+++ game.js
@@ -1,79 +1,77 @@
-var buttonColors=["red","blue","green","yellow"];
-var userClickedPattern=[];
-var started=false;
-var level=0;
-var gamePattern=[];
-$(document).keypress(function(){
-    if(!started){
-        $("#level-title").text("Level "+level);
-        nextSequence();
-        started=true;
-    }
-})
+var buttonColors = ["red", "blue", "green", "yellow"];
+var userClickedPattern = [];
+var started = false;
+var level = 0;
+var gamePattern = [];
 
-$(".btn").click(function(){
-    var clr=$(this).attr("id");
-    userClickedPattern.push(clr);
-    makeSound(clr);
-    animatePress(clr);
-    checkAnswer(userClickedPattern.length-1);
+//events
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
 });
 
-function nextSequence(){
-    userClickedPattern=[];
-    level++;
-    var randomNumber=Math.floor(Math.random()*4);
-    var randomChosenColor=buttonColors[randomNumber];
-    gamePattern.push(randomChosenColor);
-    $("#"+randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    makeSound(randomChosenColor);
+$(".btn").click(function () {
+  var clr = $(this).attr("id");
+  userClickedPattern.push(clr);
+  makeSound(clr);
+  animatePress(clr);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+//functions
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  makeSound(randomChosenColor);
 }
 
-function makeSound(name){
-    var audio=new Audio(`sounds/${name}.mp3`);
-    audio.play();
+function makeSound(name) {
+  var audio = new Audio(`sounds/${name}.mp3`);
+  audio.play();
 }
 
-function animatePress(currentColor){
-    $("#"+currentColor).addClass("pressed");
-    setTimeout(function(){$("#"+currentColor).removeClass("pressed");},100);
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 }
 
 function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("Good!");
 
-    
-    //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-
-      console.log("success");
-
-      //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
-      if (userClickedPattern.length === gamePattern.length){
-
-        //5. Call nextSequence() after a 1000 millisecond delay.
-        setTimeout(function () {
-            $("#level-title").text("Level "+level);
-          nextSequence();
-        }, 1000);
-
-      }
-
-    } else {
-
-      console.log("wrong");
-      $(document).addClass("game-over");
-      setTimeout(function(){$(document).removeClass("game-over");},200);
-      $("#level-title").text("Game Over, Press Any Key to Restart");
-      makeSound("wrong");
-      startOver();
-
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+      }, 1000);
     }
-
+  } else {
+    console.log("Its Wrong! Try Again.");
+    $(document).addClass("game-over");
+    setTimeout(function () {
+      $(document).removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    makeSound("wrong");
+    startOver();
+  }
 }
 
-function startOver(){
-    level=0;
-    started=false;
-    gamePattern=[];
-
+function startOver() {
+  level = 0;
+  started = false;
+  gamePattern = [];
 }
